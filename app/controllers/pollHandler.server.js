0c4a59b92247bb1136430cf51610f943b3d5309a
Sean <seantully00@hotmail.com>
2017-02-07 01:19:34 +0000

    saving polls

--- app/controllers/pollHandler.server.js
+++ app/controllers/pollHandler.server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Poll = require('../models/polls.js');
-var User = require('../models/users');
+var User = require('../models/users.js');
 
 function PollHandler () {
 
@@ -26,7 +26,7 @@ function PollHandler () {
 
 					//newPoll.poll.id = id;
 					newPoll.poll.question = req.body.question;
-					newPoll.poll.creator  = User.twitter.displayName;
+					newPoll.poll.creator  = User._id;
 					newPoll.poll.choices.push(req.body.choices);
 					User.polls = req.body.question;
 					//newPoll.poll.choices.count = 1;
