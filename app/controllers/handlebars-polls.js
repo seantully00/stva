63287a52fbc51c0b2ad219d24fc952fe950efe3f
Sean <seantully00@hotmail.com>
2017-03-11 02:12:52 +0000

    saving polls

---
+++ app/controllers/handlebars-polls.js
@@ -0,0 +1,15 @@
+
+var Handlebars = require('handlebars');
+
+document.addEventListener("DOMContentLoaded", function() {
+    var source = document.querySelector("#poll-template").innerHTML;
+		//Compile into a template
+		var template = Handlebars.compile(source);
+		
+		var title = "Poll Title";
+		
+		var html = template(title);
+		
+		document.querySelector("#poll=results").innerHTML = html;
+		
+});
