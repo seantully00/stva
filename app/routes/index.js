a8d3bd0723de3b389c76dc16a2f660fa483f4f0f
Sean <seantully00@hotmail.com>
2017-03-31 01:31:04 +0000

    profile

--- app/routes/index.js
+++ app/routes/index.js
@@ -43,9 +43,9 @@ module.exports = function (app, passport) {
 	app.route('/profile/:id')
 		.get(isLoggedIn, function (req, res) {
 			console.log(req.params);
-			User.find({}, function(err, user) {
+			User.find({"twitter.username":req.params.id}, function(err, user) {
 				console.log(user);
-				res.render('profile',{data:user, layout:false});
+				res.render('profile',{data:{user:user, time:Date.now()}, layout:false});
 			})
 			//res.sendFile(path + '/public/profile.html');
 		});
