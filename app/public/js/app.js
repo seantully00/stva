5803a34e5165816389fe86ae6f03aeed7ac0b149
Sean <seantully00@hotmail.com>
2017-04-13 00:51:40 +0000

    displaying polls

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,6 +1,6 @@
 function deletepoll(id) {
     console.log("delete");
     var xhr = new XMLHttpRequest();
-    xhr.open('DELETE', "/poll/" + id, false);
+    xhr.open('DELETE', "/poll/" + id, true);
     xhr.send();
 };
