16ca36e071c12d6931a4a943cfa686223879de1e
Sean <seantully00@hotmail.com>
2017-04-13 02:12:51 +0000

    displaying polls

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -4,3 +4,10 @@ function deletepoll(id) {
     xhr.open('DELETE', "/poll/" + id, true);
     xhr.send();
 };
+
+function vote(pollid, selection) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', "/poll/" + pollid, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({choice:selection}));
+};
