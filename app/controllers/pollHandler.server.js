9d35e7cb4ae614037ffce9394496ad2de044ca4f
Sean <seantully00@hotmail.com>
2017-04-16 23:42:09 +0000

    displaying polls

---
+++ app/controllers/pollHandler.server.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var User = require('../models/users')
+
+var path = process.cwd();
+//var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var PollHandler = require(path + '/app/controllers/pollHandler.server.js');
+
+module.exports = function (app, passport) {
+
+	function isLoggedIn (req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		} else {
+			res.redirect('/login');
+		}
+	}
+
+	//var clickHandler = new ClickHandler();
+	var pollHandler = new PollHandler();
+
+	app.route('/')
+		.get(isLoggedIn, function (req, res) {
+			res.sendFile(path + '/public/index.html');
+		});
+
+	app.route('/login')
+		.get(function (req, res) {
+			res.sendFile(path + '/public/login.html');
+		});
+		
+	app.route('/poll')
+		.get(function (req, res) {
+			res.sendFile(path + '/public/poll.html');
+		});	
+
+	app.route('/logout')
+		.get(function (req, res) {
+			req.logout();
+			res.redirect('/login');
+		});
+
+	app.route('/profile/:id')
+		.get(isLoggedIn, function (req, res) {
+			User.find({'twitter.username':req.params.id}, function(user) {
+				res.render('profile',{user:user});
+			})
+			//res.sendFile(path + '/public/profile.html');
+		});
+		
+	app.route('/createpoll')
+		.get(isLoggedIn, function (req, res) {
+			res.sendFile(path + '/public/createpoll.html');
+		});	
+
+	app.route('/api/:id')
+		.get(isLoggedIn, function (req, res) {
+			res.json(req.user.twitter);
+		});
+
+	app.route('/auth/github')
+		.get(passport.authenticate('github'));
+
+	app.route('/auth/github/callback')
+		.get(passport.authenticate('github', {
+			successRedirect: '/',
+			failureRedirect: '/login'
+		}));
+	
+	app.route('/auth/twitter')
+		.get(passport.authenticate('twitter'));
+
+	app.route('/auth/twitter/callback')
+		.get(passport.authenticate('twitter', {
+			successRedirect: '/',
+			failureRedirect: '/login'
+		}));	
+
+	/*app.route('/api/:id/clicks')
+		.get(isLoggedIn, clickHandler.getClicks)
+		.post(isLoggedIn, clickHandler.addClick)
+		.delete(isLoggedIn, clickHandler.resetClicks);*/
+		
+	app.route('/createpoll')
+		.post(pollHandler.addPoll);
+		
+	app.route('/success')
+		.get(function (req, res) {
+		//console.dir(req.body);
+		res.sendFile(path + '/public/success.html');
+		});
+
+		
+};
