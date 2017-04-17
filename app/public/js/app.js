a2933fb23462603576dbeeca7e2e68d10a13e9f6
Sean <seantully00@hotmail.com>
2017-04-17 17:31:11 +0000

    displaying polls

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -14,3 +14,31 @@ function vote(pollid, selection) {
 };
 
 
+function chart(pollid) {
+var xhr = new XMLHttpRequest();
+var api = 
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ,
+        datasets: [{
+            label: '# of Votes',
+            data: ,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+}
