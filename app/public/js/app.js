7869de016e55009047727bae1ad801ef859063b1
Sean <seantully00@hotmail.com>
2017-04-18 00:40:45 +0000

    displaying polls

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -21,6 +21,7 @@ var polldata;
 xhr.onreadystatechange = function() { 
         if (xhr.readyState == 4 && xhr.status == 200)
             console.log(xhr.responseText);
+            polldata = xhr.responseText;
 },
     xhr.open("GET", "https://stvapp.herokuapp.com/polldata/" + pollid, true); // true for asynchronous 
     polldata = xhr.responseText;
